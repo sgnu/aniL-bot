4813c6bf080242f8db3b69a5c8feb81101c47b94
sgnu <sam.nguyen.1223@gmail.com>
2021-08-21 13:12:49 -0400

    Add anime searching

--- app.js
+++ app.js
@@ -23,11 +23,13 @@ client.on('interactionCreate', async interaction => {
 
     if (!command) return;
 
+    await interaction.deferReply();
+
     try {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
--- commands/anime.js
+++ commands/anime.js
@@ -1,5 +1,27 @@
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { default: fetch } = require('node-fetch');
+
+const query = `
+    query ($search: String) {
+        Media(type: ANIME, search: $search, sort: POPULARITY_DESC) {
+        id
+        title {
+            romaji
+            english
+            native
+        }
+        description(asHtml: false)
+        seasonYear
+        coverImage {
+            large
+            color
+        }
+        averageScore
+        genres
+        }
+    }
+`
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,10 +29,47 @@ module.exports = {
         .setDescription('Search for an anime on AniList.')
         .addStringOption(option =>
             option.setName('search')
-            .setDescription('The title to search for')
-            .setRequired(true)
-            ),
+                .setDescription('The title to search for')
+                .setRequired(true)
+        ),
     async execute(interaction) {
-        return interaction.reply(`This will return your search... eventually`);
+        const search = interaction.options.getString('search');
+        const url = 'https://graphql.anilist.co';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                query: query,
+                variables: { 'search': search },
+            })
+        }
+
+        let index = 0;
+
+        const response = await fetch(url, options)
+            .then(response => response.json());
+
+        const anime = response.data.Media;
+        const embed = createEmbed(anime);
+
+        await interaction.editReply({ ephemeral: false, embeds: [embed] });
     }
+}
+
+function createEmbed(anime) {
+    const genres = anime.genres.length > 5 ? anime.genres.splice(5) : anime.genres
+    return new MessageEmbed()
+        .setColor(anime.coverImage.color)
+        .setTitle(anime.title.english)
+        .setURL('https://anilist.co/anime/' + anime.id)
+        .setThumbnail(anime.coverImage.large)
+        .setDescription(anime.description.replaceAll(/(<\S*>)/g, ''))
+        .addFields(
+            { name: 'Score', value: '' + (anime.averageScore / 10) + ' ⭐', inline: true },
+            { name: 'Year', value: '' + anime.seasonYear, inline: true },
+            { name: 'Genres', value: '' + genres.join(', '), inline: true}
+        )
 }
